c540018583ba427ec03e789b4a6928b5732be16c
d-ngnr <realngnr@gmail.com>
2024-05-20 17:22:37 -0500

    Add pythonic solution to reverse an array

---
+++ python/reverse_array/array_reverse_built_in.py
@@ -0,0 +1,6 @@
+def reverse_array_using_builtins (arr):
+    [print(a, end=" ") for a in list(reversed(arr))]
+    print() # removes the percent sign from the end of the string
+    
+original_array = [1,2,3,4,5,6,7,8,9]
+reverse_array_using_builtins(original_array)
--- python/reverse_array/array_reverse_simplified.py
+++ python/reverse_array/array_reverse_simplified.py
@@ -1,4 +1,5 @@
 def simplified_reverse_array(arr):
+    # starts with last element and ends with the first element with a step of -1
     for i in range(len(arr) - 1, -1, -1):
         print(arr[i], end="")
         if i != 0:
