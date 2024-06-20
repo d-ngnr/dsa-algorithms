0c4dbb1d592a3c1c7c493e9fc22a7a0b00cf32fe
d-ngnr <realngnr@gmail.com>
2024-06-20 18:46:39 -0500

    Add Solution for Jump game

---
+++ python/jump_game/jump_game.py
@@ -0,0 +1,20 @@
+def jump_to_end(arr):
+    """
+    Determines if it is possible to reach the end of the given array by jumping from one index to another.
+
+    Parameters:
+        arr (List[int]): The input array.
+
+    Returns:
+        bool: True if it is possible to reach the end of the array, False otherwise.
+    """
+    for i in range(len(arr) - 1):               # start from the first element
+        if arr[i] >= i + 1:                     # if the element is greater than or equal to the index
+            if i + arr[i] >= len(arr) - 1:      # if the index plus the element is greater than or equal to the last element
+                return True                     # return True
+        else:
+            return False                        # else return False
+
+# Example usage
+arr = [2, 3, 1, 1, 4]
+print(jump_to_end(arr))  # Output: True
