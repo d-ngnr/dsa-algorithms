691bfd9f1b7a90404d7ded1e36973c5b867b4ba8
d-ngnr <realngnr@gmail.com>
2024-08-08 22:13:58 -0500

    Add countDigits solution

--- concepts/basic_math/count_digits_in_a_number/count_digits_in_a_number.py
+++ concepts/basic_math/count_digits_in_a_number/count_digits_in_a_number.py
@@ -1,7 +1,8 @@
 def countDigits(n):
+    count = 0
     while n != 0:
-        n = n // 10 # This will remove the last digit for every iteration
         count += 1
+        n = n // 10 # This will remove the last digit for every iteration
     return count
 
 # Example usage
