64b514bfd7784641d598e3fd91ecbd33f245efa2
d-ngnr <realngnr@gmail.com>
2024-08-24 21:25:13 -0500

    Add pythonic samples

---
+++ concepts/hashing/hashing.py
@@ -0,0 +1,17 @@
+def count_occurences(arr, queries):
+    count_dict = {}
+    
+    for num in arr:
+        if num in count_dict:
+            count_dict[num] += 1
+        else:
+            count_dict[num] = 1
+    
+    for query in queries:
+        count = count_dict.get(query, 0)
+        print(f"{query}: {count}")
+
+arr = [1, 2, 1, 3, 2]
+queries = [1, 3, 4, 2, 10]
+
+count_occurences(arr, queries)
