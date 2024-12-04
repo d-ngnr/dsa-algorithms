86e6c4aae79d22f4b27612a5a2817d81e5939335
d-ngnr <realngnr@gmail.com>
2024-12-03 21:25:34 -0600

    aoc-2024 day 3 solutions

--- advent-of-code/aoc-2024/day3/day3.py
+++ advent-of-code/aoc-2024/day3/day3.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from typing import List
 
 def parse_input(filename: str) -> List[str]:
@@ -6,10 +7,35 @@ def parse_input(filename: str) -> List[str]:
         return file.readlines()
     
 def solve_part1(data: List[str]) -> int:
-    pass
+    sum = 0
+    for line in data:
+        matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)", line)
+
+        for match in matches:
+            nums = re.findall(r"\d{1,3}", match)
+            sum += int(nums[0]) * int(nums[1])
+
+    return sum
 
 def solve_part2(data: List[str]) -> int:
-    pass
+    total = 0
+    enable = True
+    for line in data:
+        pattern = re.compile(r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)')
+        instructions = re.findall(pattern, line)
+
+        for i in instructions:
+            if i == 'do()':
+                enable = True
+            elif i == 'don\'t()':
+                enable = False
+            else:
+                if enable:
+                    n1, n2 = re.findall(r'\d+', i)
+                    total += int(n1) * int(n2)
+                    
+    return total
+        
 def run_tests():
     test_data = [
         "28 25 25 22 19 16",
@@ -21,7 +47,7 @@ def run_tests():
 
 if __name__ == "__main__":
     #run_tests()
-    
+    skip = False
     input_data = parse_input('day3.txt')
     print(f"Part 1 solution: {solve_part1(input_data)}")
     print(f"Part 2 solution: {solve_part2(input_data)}")
